9fe73da4fbbffe1eb6fc0f09e23de067454c6e20
celestialorb <chris.m.busby@gmail.com>
2019-01-23 17:47:14 +0000

    Adding more suppressor tests.

--- suppressor/suppressor_test.go
+++ suppressor/suppressor_test.go
@@ -3,7 +3,9 @@ package suppressor
 import (
 	config "github.com/micro/go-config"
 	"github.com/stretchr/testify/assert"
+	apps "k8s.io/api/apps/v1"
 	core "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
 )
@@ -45,10 +47,93 @@ func TestEligibility(t *testing.T) {
 
 func TestSuppressionDecision(t *testing.T) {
 	tests := []ResourceTest{
+		// Pod without any standards.
 		ResourceTest{
 			Expected: true,
+			Resource: &core.Pod{},
+		},
+
+		// Deployment without any standards.
+		ResourceTest{
+			Expected: true,
+			Resource: &apps.Deployment{},
+		},
+
+		// DaemonSet without any standards.
+		ResourceTest{
+			Expected: true,
+			Resource: &apps.DaemonSet{},
+		},
+
+		// Pod with all proper standards.
+		ResourceTest{
+			Expected: false,
 			Resource: &core.Pod{
-				ObjectMeta: meta.ObjectMeta{},
+				ObjectMeta: meta.ObjectMeta{
+					Annotations: map[string]string{
+						"prometheus.io/scrape": "false",
+					},
+				},
+				Spec: core.PodSpec{
+					Containers: []core.Container{
+						core.Container{
+							LivenessProbe: &core.Probe{
+								Handler: core.Handler{
+									Exec: &core.ExecAction{},
+								},
+							},
+							ReadinessProbe: &core.Probe{
+								Handler: core.Handler{
+									Exec: &core.ExecAction{},
+								},
+							},
+							Resources: core.ResourceRequirements{
+								Limits: core.ResourceList{
+									core.ResourceCPU:    *resource.NewScaledQuantity(1, resource.Mega),
+									core.ResourceMemory: *resource.NewScaledQuantity(1, resource.Mega),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+
+		// Deployment with all proper standards.
+		ResourceTest{
+			Expected: false,
+			Resource: &apps.Deployment{
+				Spec: apps.DeploymentSpec{
+					Template: core.PodTemplateSpec{
+						ObjectMeta: meta.ObjectMeta{
+							Annotations: map[string]string{
+								"prometheus.io/scrape": "false",
+							},
+						},
+						Spec: core.PodSpec{
+							Containers: []core.Container{
+								core.Container{
+									LivenessProbe: &core.Probe{
+										Handler: core.Handler{
+											Exec: &core.ExecAction{},
+										},
+									},
+									ReadinessProbe: &core.Probe{
+										Handler: core.Handler{
+											Exec: &core.ExecAction{},
+										},
+									},
+									Resources: core.ResourceRequirements{
+										Limits: core.ResourceList{
+											core.ResourceCPU:    *resource.NewScaledQuantity(1, resource.Mega),
+											core.ResourceMemory: *resource.NewScaledQuantity(1, resource.Mega),
+										},
+									},
+								},
+							},
+						},
+					},
+				},
 			},
 		},
 	}
